4a1531501cef38898c4c0d253b9d6e20241cddd1
dgerin <dgerin@vsora.com>
2020-10-14 12:07:38 +0200

    add docker-compose installation control in procedure

--- build.sh
+++ build.sh
@@ -7,8 +7,12 @@ export TF_VERSION_GIT_TAG="v1.14.0"
 export BAZEL_VERSION=0.24.1
 export USE_GPU=0
 
+# install docker-compose 1.27.2 locally using pip
+pip install docker-compose==1.27.2
+USR_BIN=$(python -m site --user-base)/bin
+
 # Build the Docker image
-docker-compose build
+$USR_BIN/docker-compose build
 
 # Start the compilation
 docker-compose run tf
