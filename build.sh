b9bbe9cc09eedd374c2ca31ca5c29f5bcd535851
dgerin <dgerin@vsora.com>
2020-11-17 09:52:33 +0100

    tensorflow 1.15.4

--- build.sh
+++ build.sh
@@ -1,18 +1,13 @@
-LINUX_DISTRO="ubuntu-16.04"
+LINUX_DISTRO="ubuntu-18.04"
 cd "dist/$LINUX_DISTRO"
 
 # Set env variables
 export PYTHON_VERSION=3.6
 export TF_VERSION_GIT_TAG="v1.14.0"
-export BAZEL_VERSION=0.24.1
-export USE_GPU=0
+export BAZEL_VERSION=0.26.1
+export USE_GPU=1
 
-# install docker-compose 1.27.2 locally using pip
-pip install docker-compose==1.27.2
-USR_BIN=$(python -m site --user-base)/bin
-
-# Build the Docker image
-$USR_BIN/docker-compose build
-
-# Start the compilation
-docker-compose run tf
+## Build the Docker image
+/home/dgerin/.local/bin/docker-compose build
+## Start the compilation
+/home/dgerin/.local/bin/docker-compose run tf
