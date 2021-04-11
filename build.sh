a5401936336ad98772c9aff0a880c3dba7bb9140
Dall'Aglio, Vero <vero.dallaglio@dxc.com>
2021-04-11 23:31:33 +0200

    ubuntu-20.04

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-LINUX_DISTRO="ubuntu-16.04"
+LINUX_DISTRO="ubuntu-20.04"
 cd "dist/$LINUX_DISTRO"
 
 # Set env variables
-export PYTHON_VERSION=3.6
+export PYTHON_VERSION=3.7.5
 export TF_VERSION_GIT_TAG="v1.14.0"
 export BAZEL_VERSION=0.24.1
 export USE_GPU=0
